00d711d4d790021d1f14828b27f3f2d5ac24c9d2
Angela Chen <angchen@angchen-macbookpro.roam.corp.google.com>
2017-07-16 22:28:27 -0700

    Added new testcases for the linked list programs.

--- java_programs/DETECT_CYCLE_TEST.java
+++ java_programs/DETECT_CYCLE_TEST.java
@@ -1,19 +1,61 @@
 package java_programs;
 import java.util.*;
 
+
+/**
+ * Driver for testing DETECT_CYCLE class
+ */
 public class DETECT_CYCLE_TEST {
     public static void main(String[] args) throws Exception {
+        // Case 1: 5-node list input with no cycle
+        // Expected Output: Cycle not detected!
         Node node1 = new Node("1");
         Node node2 = new Node("2", node1);
         Node node3 = new Node("3", node2);
         Node node4 = new Node("4", node3);
         Node node5 = new Node("5", node4);
 
-        node1.setSuccessor(node2);
-
         DETECT_CYCLE dc = new DETECT_CYCLE();
 
         if (dc.detect_cycle(node5)) {
+            System.out.println("Cycle detected!");
+        } else {
+            System.out.println("Cycle not detected!");
+        }
+
+        // Case 2: 5-node list input with cycle
+        // Expected Output: Cycle detected!
+        node1.setSuccessor(node5);
+
+        if (dc.detect_cycle(node5)) {
+            System.out.println("Cycle detected!");
+        } else {
+            System.out.println("Cycle not detected!");
+        }
+
+        // Case 3: 2-node list with cycle
+        //  Expected Output: Cycle detected!
+        node1.setSuccessor(node2);
+        if (dc.detect_cycle(node2)) {
+            System.out.println("Cycle detected!");
+        } else {
+            System.out.println("Cycle not detected!");
+        }
+
+        // Case 4: 2-node list with no cycle
+        //  Expected Output: Cycle not detected!
+        Node node6 = new Node("6");
+        Node node7 = new Node("7", node6);
+        if (dc.detect_cycle(node7)) {
+            System.out.println("Cycle detected!");
+        } else {
+            System.out.println("Cycle not detected!");
+        }
+
+        // Case 5: 1-node list
+        // Expected Output: Cycle not detected!
+        Node node = new Node("0");
+        if (dc.detect_cycle(node)) {
             System.out.println("Cycle detected!");
         } else {
             System.out.println("Cycle not detected!");
--- java_programs/REVERSE_LINKED_LIST_TEST.java
+++ java_programs/REVERSE_LINKED_LIST_TEST.java
@@ -1,8 +1,13 @@
 package java_programs;
 import java.util.*;
 
-public class REVERSED_LINKED_LIST_TEST {
+/**
+ * Driver to test revers linked list.
+ */
+public class REVERSE_LINKED_LIST_TEST {
     public static void main(String[] args) throws Exception {
+        // Case 1: 5-node list input
+        // Expected Good Output: Reversed!  1 2 3 4 5
         Node node1 = new Node("1");
         Node node2 = new Node("2", node1);
         Node node3 = new Node("3", node2);
@@ -14,13 +19,46 @@ public class REVERSED_LINKED_LIST_TEST {
         Node result = rll.reverse_linked_list(node5);
 
         if (result.getValue() == node1.getValue()) {
-            System.out.println("Reversed!");
+            System.out.printf("Reversed! ");
+        } else {
+            System.out.printf("Not Reversed! ");
         }
-
         while (result != null) {
             System.out.printf("%s ", result.getValue());
             result = result.getSuccessor();
         }
         System.out.println();
+
+        // Case 2: 1-node list input
+        // Expected Output: Reversed!  0
+        Node node = new Node("0");
+        result = rll.reverse_linked_list(node);
+
+        if (result.getValue() == node.getValue()) {
+            System.out.printf("Reversed! ");
+        } else {
+            System.out.printf("Not Reversed! ");
+        }
+        while (result != null) {
+            System.out.printf("%s ", result.getValue());
+            result = result.getSuccessor();
+        }
+        System.out.println();
+
+        // Case 3: None input
+        // Expected Output: None
+        result = rll.reverse_linked_list(null);
+
+        if (result == null) {
+            System.out.printf("Reversed! ");
+        } else {
+            System.out.printf("Not Reversed! ");
+        }
+        while (result != null) {
+            System.out.printf("%s ", result.getValue());
+            result = result.getSuccessor();
+        }
+        System.out.println();
+
     }
 }
