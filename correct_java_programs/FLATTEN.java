d99da3aaa03914cc823897c0c9a7f41172b7c83d
Reza Gharibi <reza.gharibi.rg@gmail.com>
2022-08-19 23:18:26 +0430

    Fix FLATTEN indentation

--- correct_java_programs/FLATTEN.java
+++ correct_java_programs/FLATTEN.java
@@ -12,19 +12,19 @@ import java.util.*;
  */
 public class FLATTEN {
     public static Object flatten(Object arr) {
-    if (arr instanceof ArrayList) {
-        ArrayList narr = (ArrayList) arr;
-        ArrayList result = new ArrayList(50);
-        for (Object x : narr) {
-            if (x instanceof ArrayList) {
-                result.addAll((ArrayList) flatten(x));
-            } else {
-                result.add((x));
+        if (arr instanceof ArrayList) {
+            ArrayList narr = (ArrayList) arr;
+            ArrayList result = new ArrayList(50);
+            for (Object x : narr) {
+                if (x instanceof ArrayList) {
+                    result.addAll((ArrayList) flatten(x));
+                } else {
+                    result.add((x));
+                }
             }
+            return result;
+        } else {
+            return arr;
         }
-        return result;
-    } else {
-        return arr;
-    }
     }
 }
